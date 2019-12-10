c6a646335e3a78fdca4f850db1523061508e2a97
echacko <echacko@cewit.org.in>
2019-12-10 14:00:56 +0530

    Fix python-mongo count warnings

--- serve.py
+++ serve.py
@@ -12,7 +12,7 @@ from hashlib import md5
 from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash, _app_ctx_stack
 from flask_limiter import Limiter
-from werkzeug import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 import pymongo
 
 from utils import safe_pickle_dump, strip_version, isvalidid, Config
@@ -194,7 +194,7 @@ def encode_json(ps, n=10, send_images=True, send_abstracts=True):
     struct['originally_published_time'] = '%s/%s/%s' % (timestruct.month, timestruct.day, timestruct.year)
 
     # fetch amount of discussion on this paper
-    struct['num_discussion'] = comments.count({ 'pid': p['_rawid'] })
+    struct['num_discussion'] = comments.count_documents({ 'pid': p['_rawid'] })
 
     # arxiv comments from the authors (when they submit the paper)
     cc = p.get('arxiv_comment', '')
@@ -686,13 +686,13 @@ if __name__ == "__main__":
   tags_collection = mdb.tags
   goaway_collection = mdb.goaway
   follow_collection = mdb.follow
-  print('mongodb tweets_top1 collection size:', tweets_top1.count())
-  print('mongodb tweets_top7 collection size:', tweets_top7.count())
-  print('mongodb tweets_top30 collection size:', tweets_top30.count())
-  print('mongodb comments collection size:', comments.count())
-  print('mongodb tags collection size:', tags_collection.count())
-  print('mongodb goaway collection size:', goaway_collection.count())
-  print('mongodb follow collection size:', follow_collection.count())
+  print('mongodb tweets_top1 collection size:', tweets_top1.estimated_document_count())
+  print('mongodb tweets_top7 collection size:', tweets_top7.estimated_document_count())
+  print('mongodb tweets_top30 collection size:', tweets_top30.estimated_document_count())
+  print('mongodb comments collection size:', comments.estimated_document_count())
+  print('mongodb tags collection size:', tags_collection.estimated_document_count())
+  print('mongodb goaway collection size:', goaway_collection.estimated_document_count())
+  print('mongodb follow collection size:', follow_collection.estimated_document_count())
   
   TAGS = ['insightful!', 'thank you', 'agree', 'disagree', 'not constructive', 'troll', 'spam']
 
